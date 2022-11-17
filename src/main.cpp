9ccdbc92f6e1ff7342afa9307130cab393df8b2b
CDFER <cdirksfer@gmail.com>
2022-11-17 16:01:00 +1300

    update to v0.3.0

--- src/main.cpp
+++ src/main.cpp
@@ -1,84 +1,189 @@
 #include <Arduino.h>
+
+// accessPoint
 #include <DNSServer.h>
 #include <esp_wifi.h> //Used for mpdu_rx_disable android workaround
 #include <WiFi.h>
-#include <AsyncTCP.h> 	//https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
+#include <AsyncTCP.h>		   //https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
 #include <ESPAsyncWebServer.h> //ESP Async WebServer using the latest stable version from @me-no-dev
 
+const String localIPURL = "http://4.3.2.1";
+
+// const char index_html[] PROGMEM = R"=====(
+// <!DOCTYPE html> <html>
+// 	<head>
+// 	<title>ESP32 Captive Portal</title>
+// 	<style>
+// 		body {background-color:#06cc13;}
+// 		h1 {color: white;}
+// 		h2 {color: white;}
+// 	</style>
+// 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+// 	</head>
+// 	<body>
+// 	<h1>Hello World!</h1>
+// 	<h2>This is a captive portal example. All requests will be redirected here </h2>
+// 	</body>
+// </html>
+// )=====";
+
+//File: favicon.ico.gz, Size: 726
+// #define favicon_ico_gz_len 726
+// const uint8_t favicon_ico_gz[] PROGMEM = {
+//  0x1F, 0x8B, 0x08, 0x08, 0x0B, 0x87, 0x90, 0x57, 0x00, 0x03, 0x66, 0x61, 0x76, 0x69, 0x63, 0x6F,
+//  0x6E, 0x2E, 0x69, 0x63, 0x6F, 0x00, 0xCD, 0x53, 0x5F, 0x48, 0x9A, 0x51, 0x14, 0xBF, 0x62, 0x6D,
+//  0x86, 0x96, 0xA9, 0x64, 0xD3, 0xFE, 0xA8, 0x99, 0x65, 0x1A, 0xB4, 0x8A, 0xA8, 0x51, 0x54, 0x23,
+//  0xA8, 0x11, 0x49, 0x51, 0x8A, 0x34, 0x62, 0x93, 0x85, 0x31, 0x58, 0x44, 0x12, 0x45, 0x2D, 0x58,
+//  0xF5, 0x52, 0x41, 0x10, 0x23, 0x82, 0xA0, 0x20, 0x98, 0x2F, 0xC1, 0x26, 0xED, 0xA1, 0x20, 0x89,
+//  0x04, 0xD7, 0x83, 0x58, 0x20, 0x28, 0x04, 0xAB, 0xD1, 0x9B, 0x8C, 0xE5, 0xC3, 0x60, 0x32, 0x64,
+//  0x0E, 0x56, 0xBF, 0x9D, 0xEF, 0xF6, 0x30, 0x82, 0xED, 0xAD, 0x87, 0xDD, 0x8F, 0xF3, 0xDD, 0x8F,
+//  0x73, 0xCF, 0xEF, 0x9C, 0xDF, 0x39, 0xBF, 0xFB, 0x31, 0x26, 0xA2, 0x27, 0x37, 0x97, 0xD1, 0x5B,
+//  0xCF, 0x9E, 0x67, 0x30, 0xA6, 0x66, 0x8C, 0x99, 0xC9, 0xC8, 0x45, 0x9E, 0x6B, 0x3F, 0x5F, 0x74,
+//  0xA6, 0x94, 0x5E, 0xDB, 0xFF, 0xB2, 0xE6, 0xE7, 0xE7, 0xF9, 0xDE, 0xD6, 0xD6, 0x96, 0xDB, 0xD8,
+//  0xD8, 0x78, 0xBF, 0xA1, 0xA1, 0xC1, 0xDA, 0xDC, 0xDC, 0x2C, 0xEB, 0xED, 0xED, 0x15, 0x9B, 0xCD,
+//  0xE6, 0x4A, 0x83, 0xC1, 0xE0, 0x2E, 0x29, 0x29, 0x99, 0xD6, 0x6A, 0xB5, 0x4F, 0x75, 0x3A, 0x9D,
+//  0x61, 0x75, 0x75, 0x95, 0xB5, 0xB7, 0xB7, 0xDF, 0xC8, 0xD1, 0xD4, 0xD4, 0xF4, 0xB0, 0xBA, 0xBA,
+//  0xFA, 0x83, 0xD5, 0x6A, 0xFD, 0x5A, 0x5E, 0x5E, 0x9E, 0x28, 0x2D, 0x2D, 0x0D, 0x10, 0xC6, 0x4B,
+//  0x98, 0x78, 0x5E, 0x5E, 0xDE, 0x95, 0x42, 0xA1, 0x40, 0x4E, 0x4E, 0xCE, 0x65, 0x76, 0x76, 0xF6,
+//  0x47, 0xB5, 0x5A, 0x6D, 0x4F, 0x26, 0x93, 0xA2, 0xD6, 0xD6, 0x56, 0x8E, 0x6D, 0x69, 0x69, 0xD1,
+//  0x11, 0x36, 0x62, 0xB1, 0x58, 0x60, 0x32, 0x99, 0xA0, 0xD7, 0xEB, 0x51, 0x58, 0x58, 0x88, 0xFC,
+//  0xFC, 0x7C, 0x10, 0x16, 0x02, 0x56, 0x2E, 0x97, 0x43, 0x2A, 0x95, 0x42, 0x2C, 0x16, 0x23, 0x33,
+//  0x33, 0x33, 0xAE, 0x52, 0xA9, 0x1E, 0x64, 0x65, 0x65, 0x71, 0x7C, 0x7D, 0x7D, 0xBD, 0x93, 0xEA,
+//  0xFE, 0x30, 0x1A, 0x8D, 0xE8, 0xEC, 0xEC, 0xC4, 0xE2, 0xE2, 0x22, 0x6A, 0x6A, 0x6A, 0x40, 0x39,
+//  0x41, 0xB5, 0x38, 0x4E, 0xC8, 0x33, 0x3C, 0x3C, 0x0C, 0x87, 0xC3, 0xC1, 0x6B, 0x54, 0x54, 0x54,
+//  0xBC, 0xE9, 0xEB, 0xEB, 0x93, 0x5F, 0x5C, 0x5C, 0x30, 0x8A, 0x9D, 0x2E, 0x2B, 0x2B, 0xBB, 0xA2,
+//  0x3E, 0x41, 0xBD, 0x21, 0x1E, 0x8F, 0x63, 0x6A, 0x6A, 0x0A, 0x81, 0x40, 0x00, 0x94, 0x1B, 0x3D,
+//  0x3D, 0x3D, 0x42, 0x3C, 0x96, 0x96, 0x96, 0x70, 0x7E, 0x7E, 0x8E, 0xE3, 0xE3, 0x63, 0xF8, 0xFD,
+//  0xFE, 0xB4, 0xD7, 0xEB, 0xF5, 0x8F, 0x8F, 0x8F, 0x5B, 0x68, 0x5E, 0x6F, 0x05, 0xCE, 0xB4, 0xE3,
+//  0xE8, 0xE8, 0x08, 0x27, 0x27, 0x27, 0xD8, 0xDF, 0xDF, 0xC7, 0xD9, 0xD9, 0x19, 0x6C, 0x36, 0x1B,
+//  0x36, 0x36, 0x36, 0x38, 0x9F, 0x85, 0x85, 0x05, 0xAC, 0xAF, 0xAF, 0x23, 0x1A, 0x8D, 0x22, 0x91,
+//  0x48, 0x20, 0x16, 0x8B, 0xFD, 0xDA, 0xDA, 0xDA, 0x7A, 0x41, 0x33, 0x7E, 0x57, 0x50, 0x50, 0x80,
+//  0x89, 0x89, 0x09, 0x84, 0xC3, 0x61, 0x6C, 0x6F, 0x6F, 0x23, 0x12, 0x89, 0xE0, 0xE0, 0xE0, 0x00,
+//  0x43, 0x43, 0x43, 0x58, 0x5E, 0x5E, 0xE6, 0x9C, 0x7D, 0x3E, 0x1F, 0x46, 0x47, 0x47, 0x79, 0xBE,
+//  0xBD, 0xBD, 0x3D, 0xE1, 0x3C, 0x1D, 0x0C, 0x06, 0x9F, 0x10, 0xB7, 0xC7, 0x84, 0x4F, 0xF6, 0xF7,
+//  0xF7, 0x63, 0x60, 0x60, 0x00, 0x83, 0x83, 0x83, 0x18, 0x19, 0x19, 0xC1, 0xDC, 0xDC, 0x1C, 0x8F,
+//  0x17, 0x7C, 0xA4, 0x27, 0xE7, 0x34, 0x39, 0x39, 0x89, 0x9D, 0x9D, 0x1D, 0x6E, 0x54, 0xE3, 0x13,
+//  0xE5, 0x34, 0x11, 0x37, 0x49, 0x51, 0x51, 0xD1, 0x4B, 0xA5, 0x52, 0xF9, 0x45, 0x26, 0x93, 0x5D,
+//  0x0A, 0xF3, 0x92, 0x48, 0x24, 0xA0, 0x6F, 0x14, 0x17, 0x17, 0xA3, 0xB6, 0xB6, 0x16, 0x5D, 0x5D,
+//  0x5D, 0x7C, 0x1E, 0xBB, 0xBB, 0xBB, 0x9C, 0xD7, 0xE1, 0xE1, 0x21, 0x42, 0xA1, 0xD0, 0x6B, 0xD2,
+//  0x45, 0x4C, 0x33, 0x12, 0x34, 0xCC, 0xA0, 0x19, 0x54, 0x92, 0x56, 0x0E, 0xD2, 0xD9, 0x43, 0xF8,
+//  0xCF, 0x82, 0x56, 0xC2, 0xDC, 0xEB, 0xEA, 0xEA, 0x38, 0x7E, 0x6C, 0x6C, 0x4C, 0xE0, 0xFE, 0x9D,
+//  0xB8, 0xBF, 0xA7, 0xFA, 0xAF, 0x56, 0x56, 0x56, 0xEE, 0x6D, 0x6E, 0x6E, 0xDE, 0xB8, 0x47, 0x55,
+//  0x55, 0x55, 0x6C, 0x66, 0x66, 0x46, 0x44, 0xDA, 0x3B, 0x34, 0x1A, 0x4D, 0x94, 0xB0, 0x3F, 0x09,
+//  0x7B, 0x45, 0xBD, 0xA5, 0x5D, 0x2E, 0x57, 0x8C, 0x7A, 0x73, 0xD9, 0xED, 0xF6, 0x3B, 0x84, 0xFF,
+//  0xE7, 0x7D, 0xA6, 0x3A, 0x2C, 0x95, 0x4A, 0xB1, 0x8E, 0x8E, 0x0E, 0x6D, 0x77, 0x77, 0xB7, 0xCD,
+//  0xE9, 0x74, 0x3E, 0x73, 0xBB, 0xDD, 0x8F, 0x3C, 0x1E, 0x8F, 0xE6, 0xF4, 0xF4, 0x94, 0xAD, 0xAD,
+//  0xAD, 0xDD, 0xDE, 0xCF, 0x73, 0x0B, 0x0B, 0xB8, 0xB6, 0xE0, 0x5D, 0xC6, 0x66, 0xC5, 0xE4, 0x10,
+//  0x4C, 0xF4, 0xF7, 0xD8, 0x59, 0xF2, 0x7F, 0xA3, 0xB8, 0xB4, 0xFC, 0x0F, 0xEE, 0x37, 0x70, 0xEC,
+//  0x16, 0x4A, 0x7E, 0x04, 0x00, 0x00
+// };
+
+// Onboard Flash Storage
+#include <SPIFFS.h>
+
+
+
+// Debug
 #define DEBUG_SERIAL if(USE_SERIAL)Serial //don't touch, enable serial in platformio.ini
 
-DNSServer dnsServer;
-AsyncWebServer server(80);
+void accessPoint(void *parameter){
+	#define DNS_INTERVAL 10 //ms between processing dns requests: dnsServer.processNextRequest();
 
-void setup(){ //the order of the code is really important and it is critical the the android workaround is after the dns and sofAP setup
+	const char * ssid = "captive"; //FYI The SSID can't have a space in it.
+	const char * password = "12345678";
 
-  #if USE_SERIAL == true
-  Serial.begin(115200);
-  while (!Serial);
-  Serial.println("\n\nCaptive Test, compiled " __DATE__ " " __TIME__ "by CD_FER");
-  #endif
+	#define MAX_CLIENTS 4
+	#define WIFI_CHANNEL 6 //2.4ghz channel 6
 
-  WiFi.mode(WIFI_AP);
-  WiFi.softAPConfig(IPAddress(4, 3, 2, 1), IPAddress(4, 3, 2, 1), IPAddress(255, 255, 255, 0)); //Samsung requires the IP to be in public space
-  WiFi.softAP("captive", "12345678", 6, 0, 4); //2.4ghz channel 6, do broadcast SSID (0), 4 max clients, FYI The SSID in SoftAP can't have a space in it.
-  
-  dnsServer.setErrorReplyCode(DNSReplyCode::NoError); //not sure if this is necessary
-  dnsServer.setTTL(300); //set 5min client side cache for DNS
-  // if DNSServer is started with "*" for domain name, it will reply with provided IP to all DNS request
-  dnsServer.start(53, "*", WiFi.softAPIP());
 
-  //ampdu_rx_disable android workaround see https://github.com/espressif/arduino-esp32/issues/4423
-  esp_wifi_stop();
-  esp_wifi_deinit();
+	const IPAddress localIP(4, 3, 2, 1); // the IP address the web server, Samsung requires the IP to be in public space
+	const IPAddress gatewayIP(4, 3, 2, 1); // IP address of the network
+	const IPAddress subnetMask(255,255,255,0);
 
-  wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();   //We use the default config ...
-  my_config.ampdu_rx_enable = false;                           //... and modify only what we want.
+	DNSServer dnsServer;
+	AsyncWebServer server(80);
 
-  esp_wifi_init(&my_config); //set the new config
-  esp_wifi_start(); //Restart WiFi
-  delay(100); //this seems to be necessary don't ask me why
+	WiFi.mode(WIFI_AP);
+	WiFi.softAPConfig(localIP, gatewayIP, subnetMask); //Samsung requires the IP to be in public space
+	WiFi.softAP(ssid, password, WIFI_CHANNEL, 0, MAX_CLIENTS);
+	
+	dnsServer.setTTL(300); //set 5min client side cache for DNS
+	dnsServer.start(53, "*", localIP); //if DNSServer is started with "*" for domain name, it will reply with provided IP to all DNS request
 
-  //Required
-  server.on("/connecttest.txt",[](AsyncWebServerRequest *request){request->redirect("http://logout.net/");}); //windows 11 captive portal workaround
+	//ampdu_rx_disable android workaround see https://github.com/espressif/arduino-esp32/issues/4423
+	esp_wifi_stop();
+	esp_wifi_deinit();
 
-  //Probably not Required, but might speed things up?
-  server.on("/canonical.html",[](AsyncWebServerRequest *request){request->redirect("http://4.3.2.1/");}); //firefox captive portal call home
-  server.on("/chrome-variations/seed",[](AsyncWebServerRequest *request){request->send(200);}); //chrome captive portal call home
-  server.on("/redirect",[](AsyncWebServerRequest *request){request->redirect("http://4.3.2.1/");}); //microsoft redirect
-  server.on("/success.txt",[](AsyncWebServerRequest *request){request->send(200);}); //firefox captive portal call home
-  server.on("/wpad.dat",[](AsyncWebServerRequest *request){request->send(404);}); //Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
-  server.on("/generate_204",[](AsyncWebServerRequest *request){request->redirect("http://4.3.2.1/");}); //chromium? android? captive portal redirect
-  server.on("/service/update2/json",[](AsyncWebServerRequest *request){request->send(200);}); //firefox?
-  server.on("/chat",[](AsyncWebServerRequest *request){request->send(404);}); //No stop asking Whatsapp, there is no internet connection
+	wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();   //We use the default config ...
+	my_config.ampdu_rx_enable = false;                           //... and modify only what we want.
 
-  //return 404 to webpage icon
-  server.on("/favicon.ico",[](AsyncWebServerRequest *request){request->send(404);}); //webpage icon
+	esp_wifi_init(&my_config); //set the new config
+	esp_wifi_start(); //Restart WiFi
+	delay(100); //this is necessary don't ask me why
 
-  //Serve Basic HTML Page WARNING IOS (and maybe macos) WILL NOT POP UP IF THIS PAGE CONTAINS THE WORD "Success" https://www.esp8266.com/viewtopic.php?f=34&t=4398
-  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
-    AsyncWebServerResponse *response = request->beginResponse(200, "text/html", "<!DOCTYPE html><html><head><title>ESP32 Captive Portal</title></head><body><p>This is a captive portal example. All requests will "
-  "be redirected here.</p></body>");
-    response->addHeader("Cache-Control", "public,max-age=31536000");
-    request->send(response);
-    DEBUG_SERIAL.println("Served Basic HTML Page with 1 year Cache header");
-  });
+	//Required
+	server.on("/connecttest.txt",[](AsyncWebServerRequest *request){request->redirect("http://logout.net");}); //windows 11 captive portal workaround
 
-  server.onNotFound([](AsyncWebServerRequest *request){
-    request->redirect("http://4.3.2.1/");
-    DEBUG_SERIAL.print("server.onnotfound ");
-    DEBUG_SERIAL.print(request->host());       //This gives some insight into whatever was being requested on the serial monitor
-    DEBUG_SERIAL.print(request->url());
-    DEBUG_SERIAL.print(" sent redirect to http://4.3.2.1/\n");
-  });
+	//Probably not all are Required, but some are. Others might speed things up?
+	server.on("/canonical.html",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //firefox captive portal call home
+	server.on("/chrome-variations/seed",[](AsyncWebServerRequest *request){request->send(200);}); //chrome captive portal call home
+	server.on("/redirect",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //microsoft redirect
+	server.on("/success.txt",[](AsyncWebServerRequest *request){request->send(200);}); //firefox captive portal call home
+	server.on("/wpad.dat",[](AsyncWebServerRequest *request){request->send(404);}); //Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
+	server.on("/generate_204",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //chromium? android? captive portal redirect
+	server.on("/service/update2/json",[](AsyncWebServerRequest *request){request->send(200);}); //firefox?
+	server.on("/chat",[](AsyncWebServerRequest *request){request->send(404);}); //No stop asking Whatsapp, there is no internet connection
+	server.on("/startpage",[](AsyncWebServerRequest *request){request->redirect(localIPURL);});
 
-  server.begin();
 
-  DEBUG_SERIAL.print("\n");
-  DEBUG_SERIAL.print("Startup Time:"); //should be somewhere between 270-350 for Generic ESP32 (D0WDQ6 chip, can have a higher startup time on first boot)
-  DEBUG_SERIAL.println(millis());
-  DEBUG_SERIAL.print("\n");
+	//return webpage icon
+	server.on("/favicon.ico", HTTP_GET, [](AsyncWebServerRequest *request){
+		AsyncWebServerResponse *response = request->beginResponse_P(200, "image/x-icon", favicon_ico_gz, favicon_ico_gz_len);
+		response->addHeader("Content-Encoding", "gzip");
+		request->send(response);
+		DEBUG_SERIAL.print("Sent Favicon\n");
+	});
+
+	//Serve Basic HTML Page WARNING IOS (and maybe macos) WILL NOT POP UP IF THIS PAGE CONTAINS THE WORD "Success" https://www.esp8266.com/viewtopic.php?f=34&t=4398
+	server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+		AsyncWebServerResponse *response = request->beginResponse(200, "text/html", index_html);
+		response->addHeader("Cache-Control", "public,max-age=31536000");
+		request->send(response);
+		DEBUG_SERIAL.print("Served Basic HTML Page\n");
+	});
+
+	server.onNotFound([](AsyncWebServerRequest *request){
+		request->redirect(localIPURL);
+		DEBUG_SERIAL.print("onnotfound ");
+		DEBUG_SERIAL.print(request->host());       //This gives some insight into whatever was being requested on the serial monitor
+		DEBUG_SERIAL.print(" ");
+		DEBUG_SERIAL.print(request->url());
+		DEBUG_SERIAL.print(" sent redirect to " + localIPURL +"\n");
+	});
+
+	server.begin();
+
+	while (true){
+		dnsServer.processNextRequest();
+		vTaskDelay(DNS_INTERVAL / portTICK_PERIOD_MS);
+	}
+}
+
+void setup(){
+	#if USE_SERIAL == true
+		Serial.begin(115200);
+		while (!Serial);
+		Serial.println("\n\nBase ESP32 Project, compiled " __DATE__ " " __TIME__ "by CD_FER");
+	#endif
+
+	if (!SPIFFS.begin(true)){
+		DEBUG_SERIAL.print("can't mount SPIFFS");
+    }
+
+	// 			Function, Name (for debugging), Stack size, Params, Priority, Handle
+	xTaskCreate(accessPoint, "accessPoint", 5000, NULL, 1, NULL);
 }
 
 void loop(){
-  dnsServer.processNextRequest();
+	vTaskDelay(1000 / portTICK_PERIOD_MS); //Keep RTOS Happy when nothing is here
 }
