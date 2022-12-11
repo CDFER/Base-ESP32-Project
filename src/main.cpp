7df8391db6050b6e9c615edb91247b513591a38a
CDFER <cdirksfer@gamil.com>
2022-12-11 14:42:28 +1300

    Format Code

--- src/main.cpp
+++ src/main.cpp
@@ -1,86 +1,82 @@
 #include <Arduino.h>
 
 // accessPoint
+#include <AsyncTCP.h>  //https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
 #include <DNSServer.h>
-#include <esp_wifi.h> //Used for mpdu_rx_disable android workaround
+#include <ESPAsyncWebServer.h>	//ESP Async WebServer using the latest stable version from @me-no-dev
 #include <WiFi.h>
-#include <AsyncTCP.h>		   //https://github.com/me-no-dev/AsyncTCP using the latest dev version from @me-no-dev
-#include <ESPAsyncWebServer.h> //ESP Async WebServer using the latest stable version from @me-no-dev
+#include <esp_wifi.h>  //Used for mpdu_rx_disable android workaround
 
-const char * ssid = "captive"; //FYI The SSID can't have a space in it.
-const char * password = "12345678";
-const IPAddress localIP(4, 3, 2, 1); // the IP address the webserver, Samsung requires the IP to be in public space
-const IPAddress gatewayIP(4, 3, 2, 1); // IP address of the network
-const String localIPURL = "http://4.3.2.1/index.html"; //URL to the webserver
-
-
+const char *ssid = "captive";  // FYI The SSID can't have a space in it.
+const char *password = "12345678";
+const IPAddress localIP(4, 3, 2, 1);					// the IP address the webserver, Samsung requires the IP to be in public space
+const IPAddress gatewayIP(4, 3, 2, 1);					// IP address of the network
+const String localIPURL = "http://4.3.2.1/index.html";	// URL to the webserver
 
 // Onboard Flash Storage
-#include <SPIFFS.h> //LittleFS was tested but I don't think it is as stable as SPIFFS on the ESP32 but I may change it in the future
+#include <SPIFFS.h>	 //LittleFS was tested but I don't think it is as stable as SPIFFS on the ESP32 but I may change it in the future
 
-void accessPoint(void *parameter){
-	#define DNS_INTERVAL 10 //ms between processing dns requests: dnsServer.processNextRequest();
+void accessPoint(void *parameter) {
+  #define DNS_INTERVAL 10	 // ms between processing dns requests: dnsServer.processNextRequest();
 
-	#define MAX_CLIENTS 4
-	#define WIFI_CHANNEL 6 //2.4ghz channel 6
+  #define MAX_CLIENTS 4
+  #define WIFI_CHANNEL 6	// 2.4ghz channel 6
 
-	const IPAddress subnetMask(255,255,255,0);
+	const IPAddress subnetMask(255, 255, 255, 0);
 
 	DNSServer dnsServer;
 	AsyncWebServer server(80);
 
 	WiFi.mode(WIFI_AP);
-	WiFi.softAPConfig(localIP, gatewayIP, subnetMask); //Samsung requires the IP to be in public space
+	WiFi.softAPConfig(localIP, gatewayIP, subnetMask);	// Samsung requires the IP to be in public space
 	WiFi.softAP(ssid, password, WIFI_CHANNEL, 0, MAX_CLIENTS);
 	WiFi.setSleep(false);
-	
-	dnsServer.setTTL(300); //set 5min client side cache for DNS
-	dnsServer.start(53, "*", localIP); //if DNSServer is started with "*" for domain name, it will reply with provided IP to all DNS request
 
-	//ampdu_rx_disable android workaround see https://github.com/espressif/arduino-esp32/issues/4423
+	dnsServer.setTTL(300);				// set 5min client side cache for DNS
+	dnsServer.start(53, "*", localIP);	// if DNSServer is started with "*" for domain name, it will reply with provided IP to all DNS request
+
+	// ampdu_rx_disable android workaround see https://github.com/espressif/arduino-esp32/issues/4423
 	esp_wifi_stop();
 	esp_wifi_deinit();
 
-	wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();   //We use the default config ...
-	my_config.ampdu_rx_enable = false;                           //... and modify only what we want.
+	wifi_init_config_t my_config = WIFI_INIT_CONFIG_DEFAULT();	// We use the default config ...
+	my_config.ampdu_rx_enable = false;							//... and modify only what we want.
 
-	esp_wifi_init(&my_config); //set the new config
-	esp_wifi_start(); //Restart WiFi
-	delay(100); //this is necessary don't ask me why
-
+	esp_wifi_init(&my_config);	// set the new config
+	esp_wifi_start();			// Restart WiFi
+	delay(100);					// this is necessary don't ask me why
 
 	//======================== Webserver ========================
-	//WARNING IOS (and maybe macos) WILL NOT POP UP IF IT CONTAINS THE WORD "Success" https://www.esp8266.com/viewtopic.php?f=34&t=4398
-	//SAFARI (IOS) IS STUPID, G-ZIPPED FILES CAN'T END IN .GZ https://github.com/homieiot/homie-esp8266/issues/476
-	//SAFARI (IOS) there is a 128KB limit to the size of the HTML. The HTML can reference external resources/images that bring the total over 128KB
-	//SAFARI (IOS) popup browser has some severe limitations (javascript disabled, cookies disabled) 
+	// WARNING IOS (and maybe macos) WILL NOT POP UP IF IT CONTAINS THE WORD "Success" https://www.esp8266.com/viewtopic.php?f=34&t=4398
+	// SAFARI (IOS) IS STUPID, G-ZIPPED FILES CAN'T END IN .GZ https://github.com/homieiot/homie-esp8266/issues/476
+	// SAFARI (IOS) there is a 128KB limit to the size of the HTML. The HTML can reference external resources/images that bring the total over 128KB
+	// SAFARI (IOS) popup browser has some severe limitations (javascript disabled, cookies disabled)
 
-	server.serveStatic("/Water_Quality_Data.csv", SPIFFS, "/Water_Quality_Data.csv").setCacheControl("no-store"); //fetch data file every page reload
-	server.serveStatic("/index.html", SPIFFS, "/index.html").setCacheControl("max-age=120"); //serve html file
+	server.serveStatic("/Water_Quality_Data.csv", SPIFFS, "/Water_Quality_Data.csv").setCacheControl("no-store");  // fetch data file every page reload
+	server.serveStatic("/index.html", SPIFFS, "/index.html").setCacheControl("max-age=120");					   // serve html file
 
-	//Required
-	server.on("/connecttest.txt",[](AsyncWebServerRequest *request){request->redirect("http://logout.net");}); //windows 11 captive portal workaround
-	server.on("/wpad.dat",[](AsyncWebServerRequest *request){request->send(404);}); //Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
+	// Required
+	server.on("/connecttest.txt", [](AsyncWebServerRequest *request) { request->redirect("http://logout.net"); });	// windows 11 captive portal workaround
+	server.on("/wpad.dat", [](AsyncWebServerRequest *request) { request->send(404); });								// Honestly don't understand what this is but a 404 stops win 10 keep calling this repeatedly and panicking the esp32 :)
 
+	// Background responses: Probably not all are Required, but some are. Others might speed things up?
+	// A Tier (commonly used by modern systems)
+	server.on("/generate_204", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });		   // android captive portal redirect
+	server.on("/redirect", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });			   // microsoft redirect
+	server.on("/hotspot-detect.html", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });  // apple call home
+	server.on("/canonical.html", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });	   // firefox captive portal call home
+	server.on("/success.txt", [](AsyncWebServerRequest *request) { request->send(200); });					   // firefox captive portal call home
+	server.on("/ncsi.txt", [](AsyncWebServerRequest *request) { request->redirect(localIPURL); });			   // windows call home
 
-	//Background responses: Probably not all are Required, but some are. Others might speed things up?
-	//A Tier (commonly used by modern systems)
-	server.on("/generate_204",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); // android captive portal redirect
-	server.on("/redirect",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //microsoft redirect
-	server.on("/hotspot-detect.html",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //apple call home
-	server.on("/canonical.html",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //firefox captive portal call home
-	server.on("/success.txt",[](AsyncWebServerRequest *request){request->send(200);}); //firefox captive portal call home
-	server.on("/ncsi.txt",[](AsyncWebServerRequest *request){request->redirect(localIPURL);}); //windows call home
+	// B Tier (uncommon)
+	//  server.on("/chrome-variations/seed",[](AsyncWebServerRequest *request){request->send(200);}); //chrome captive portal call home
+	//  server.on("/service/update2/json",[](AsyncWebServerRequest *request){request->send(200);}); //firefox?
+	//  server.on("/chat",[](AsyncWebServerRequest *request){request->send(404);}); //No stop asking Whatsapp, there is no internet connection
+	//  server.on("/startpage",[](AsyncWebServerRequest *request){request->redirect(localIPURL);});
 
-	//B Tier (uncommon)
-	// server.on("/chrome-variations/seed",[](AsyncWebServerRequest *request){request->send(200);}); //chrome captive portal call home
-	// server.on("/service/update2/json",[](AsyncWebServerRequest *request){request->send(200);}); //firefox?
-	// server.on("/chat",[](AsyncWebServerRequest *request){request->send(404);}); //No stop asking Whatsapp, there is no internet connection
-	// server.on("/startpage",[](AsyncWebServerRequest *request){request->redirect(localIPURL);});
+	server.serveStatic("/", SPIFFS, "/").setCacheControl("max-age=120").setDefaultFile("index.html");  // serve any file on the device when requested
 
-	server.serveStatic("/", SPIFFS, "/").setCacheControl("max-age=120").setDefaultFile("index.html"); //serve any file on the device when requested
-
-	server.onNotFound([](AsyncWebServerRequest *request){
+	server.onNotFound([](AsyncWebServerRequest *request) {
 		request->redirect(localIPURL);
 		ESP_LOGW("WebServer", "Page not found sent redirect to localIPURL");
 		// DEBUG_SERIAL.print("onnotfound ");
@@ -94,30 +90,28 @@ void accessPoint(void *parameter){
 
 	ESP_LOGI("WebServer", "Startup complete");
 
-	while (true){
+	while (true) {
 		dnsServer.processNextRequest();
 		vTaskDelay(DNS_INTERVAL / portTICK_PERIOD_MS);
 	}
 }
 
+void setup() {
+  #if USE_SERIAL == true
+	Serial.begin(115200);
+	while (!Serial)
+	  ;
+	ESP_LOGI("Base ESP32 Project", "Compiled " __DATE__ " " __TIME__ " by CD_FER");
+  #endif
 
-
-void setup(){
-	#if USE_SERIAL == true
-		Serial.begin(115200);
-		while (!Serial);
-		ESP_LOGI("Base ESP32 Project", "Compiled " __DATE__ " " __TIME__ " by CD_FER");
-	#endif
-
-
-	if (!SPIFFS.begin(true)){
+	if (!SPIFFS.begin(true)) {
 		ESP_LOGE("File System Error", "Can't mount SPIFFS");
-    }
+	}
 
 	// 			Function, Name (for debugging), Stack size, Params, Priority, Handle
 	xTaskCreate(accessPoint, "accessPoint", 5000, NULL, 1, NULL);
 }
 
-void loop(){
-	vTaskDelay(1000 / portTICK_PERIOD_MS); //Keep RTOS Happy when nothing is here
+void loop() {
+	vTaskDelay(1000 / portTICK_PERIOD_MS);	// Keep RTOS Happy when nothing is here
 }
